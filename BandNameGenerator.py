0d9632c4fd161b499b4a6726bdea78fad14d43c3
JayCodes12 <chapmanj168@gmail.com>
2025-07-03 15:43:20 -0400

    Update BandNameGenerator.py
    Added "\n" and ":" to inputs and prints

--- BandNameGenerator.py
+++ BandNameGenerator.py
@@ -1,11 +1,11 @@
 print("Hello! Welcome to the Band Name Generator!")
 
 #2
-user_city = input("What city did you grow up in? ")
+user_city = input("What city did you grow up in? \n")
 
 #3
-pet_name = input("What is the name of your pet? ")
+pet_name = input("What is the name of your pet? \n")
 
 band_name = (user_city + " " + pet_name)
 
-print("Your band name is " + band_name)
+print("Your band name is: " + band_name)
