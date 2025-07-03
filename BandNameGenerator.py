ed911a5d64e0faecf654173fc8f650d0cf1e612e
JayCodes12 <chapmanj168@gmail.com>
2025-07-02 23:18:48 -0400

    Add files via upload
    This is the correct syntax for the Band Name Generator project. There are multiple ways to write this program.

---
+++ BandNameGenerator.py
@@ -0,0 +1,11 @@
+print("Hello! Welcome to the Band Name Generator!")
+
+#2
+user_city = input("What city did you grow up in? ")
+
+#3
+pet_name = input("What is the name of your pet? ")
+
+band_name = (user_city + " " + pet_name)
+
+print("Your band name is " + band_name)
